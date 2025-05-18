791865c9a5712ce35207b63ec7b9268f6fc42bb9
mlarause <mlara681@misena.edu.co>
2025-05-18 18:50:01 -0500

    token 2

--- controllers/supplierController.js
+++ controllers/supplierController.js
@@ -1,25 +1,27 @@
-const Supplier = require('../models/Supplier');
+const Supplier = require("../models/Supplier");
+const { validatePermissions } = require("../utils/roleValidation");
 
-const getAllSuppliers = async (req, res) => {
+// Crear proveedor (Admin o Coordinador)
+exports.createSupplier = async (req, res) => {
   try {
-    const suppliers = await Supplier.find();
-    res.json(suppliers);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+    validatePermissions(req.user.role, ["admin", "coordinador"]);
 
-const createSupplier = async (req, res) => {
-  try {
-    const newSupplier = new Supplier(req.body);
-    await newSupplier.save();
-    res.status(201).json(newSupplier);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-};
+    const { name, contact, address } = req.body;
+    const newSupplier = await Supplier.create({
+      name,
+      contact,
+      address,
+      createdBy: req.user.id,
+    });
 
-module.exports = {
-  getAllSuppliers,
-  createSupplier
+    res.status(201).json({
+      success: true,
+      data: newSupplier,
+    });
+  } catch (error) {
+    res.status(error.status || 500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
