f5c8d74e53c7ae890c1b5561c8f6ef1d6522d244
mlarause <mlara681@misena.edu.co>
2025-05-18 19:11:54 -0500

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,18 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { verifyToken } = require('../middlewares/authJwt');
+const { verifyToken, isAdmin } = require('../middlewares/authJwt');
 const userController = require('../controllers/userController');
 
-// Obtener todos los usuarios (solo admin)
-router.get('/', [verifyToken], userController.getAllUsers);
-
-// Obtener un usuario por ID
-router.get('/:id', [verifyToken], userController.getUserById);
-
-// Actualizar usuario
-router.put('/:id', [verifyToken], userController.updateUser);
-
-// Eliminar usuario (solo admin)
-router.delete('/:id', [verifyToken], userController.deleteUser);
+router.get('/', [verifyToken, isAdmin], userController.getAllUsers);
+router.get('/:id', verifyToken, userController.getUserById);
+router.put('/:id', verifyToken, userController.updateUser);
+router.delete('/:id', [verifyToken, isAdmin], userController.deleteUser);
 
 module.exports = router;
