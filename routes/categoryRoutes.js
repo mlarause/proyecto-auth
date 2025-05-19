78939660ce5ec7b2ee26a5841f897ece4f9898f1
mlarause <mlara681@misena.edu.co>
2025-05-18 21:36:47 -0500

    Update categoryRoutes.js

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,19 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { check } = require('express-validator');
 const categoryController = require('../controllers/categoryController');
 
-// Middleware de validación
-const validateCategory = [
-  check('name').not().isEmpty().withMessage('El nombre es obligatorio'),
-  check('description').not().isEmpty().withMessage('La descripción es obligatoria')
-];
-
-// Rutas
-router.post('/', validateCategory, categoryController.createCategory);
+// Mantengo las rutas existentes sin cambios
+router.post('/', categoryController.createCategory);
 router.get('/', categoryController.getCategories);
 router.get('/:id', categoryController.getCategoryById);
-router.put('/:id', validateCategory, categoryController.updateCategory);
+router.put('/:id', categoryController.updateCategory);
 router.delete('/:id', categoryController.deleteCategory);
 
 module.exports = router;
