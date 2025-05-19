cc97cadeec38806778388bc6184520e30e5ffc69
mlarause <mlara681@misena.edu.co>
2025-05-19 12:40:20 -0500

    Update supplierRoutes.js

--- routes/supplierRoutes.js
+++ routes/supplierRoutes.js
@@ -1,38 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const supplierController = require('../controllers/supplierController');
-const authMiddleware = require('../middlewares/auth'); // Importación corregida
+const { verifyToken, isAdmin, isCoordinador } = require('../middlewares/auth');
 
-// CREATE - Admin y Coordinador
+// CREATE - Admin y Coordinador (igual que en categorías)
 router.post('/',
-  authMiddleware.verifyToken, // Usar authMiddleware.verifyToken
-  authMiddleware.checkRole(['admin', 'coordinador']),
+  verifyToken,
+  isCoordinador,
   supplierController.createSupplier
 );
 
 // READ ALL - Todos autenticados
 router.get('/',
-  authMiddleware.verifyToken,
+  verifyToken,
   supplierController.getAllSuppliers
 );
 
 // READ ONE - Todos autenticados
 router.get('/:id',
-  authMiddleware.verifyToken,
+  verifyToken,
   supplierController.getSupplierById
 );
 
 // UPDATE - Admin y Coordinador
 router.put('/:id',
-  authMiddleware.verifyToken,
-  authMiddleware.checkRole(['admin', 'coordinador']),
+  verifyToken,
+  isCoordinador,
   supplierController.updateSupplier
 );
 
 // DELETE - Solo Admin
 router.delete('/:id',
-  authMiddleware.verifyToken,
-  authMiddleware.isAdmin,
+  verifyToken,
+  isAdmin,
   supplierController.deleteSupplier
 );
 
