1297d6cb54d43c19ce5cb3a183373900a42891a0
mlarause <mlara681@misena.edu.co>
2025-05-19 10:00:12 -0500

    Update role.js

--- middlewares/role.js
+++ middlewares/role.js
@@ -1,13 +1,13 @@
-function checkRole(allowedRoles) {
+const checkRole = (allowedRoles) => {
   return (req, res, next) => {
-    if (!allowedRoles.includes(req.user.role)) {
-      return res.status(403).json({ 
-        success: false, 
-        message: 'Acceso denegado: permisos insuficientes' 
+    if (!allowedRoles.includes(req.userRole)) {
+      return res.status(403).json({
+        success: false,
+        message: `Acceso denegado. Rol requerido: ${allowedRoles.join(", ")}`
       });
     }
     next();
   };
-}
+};
 
-module.exports = { checkRole };
+module.exports = checkRole;
