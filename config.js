60968ad568058749b2f414e96e264d02630fba6b
mlarause <mlara681@misena.edu.co>
2025-05-18 19:32:50 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  SECRET: 'tu_clave_secreta_jwt_aqui', // Cambiar en producción
-  MONGODB_URI: 'mongodb://127.0.0.1:27017/proyecto-auth'
+  SECRET: 'tusecretoparalostokens', // DEBE COINCIDIR CON LA USADA AL GENERAR EL TOKEN
+  MONGODB_URI: 'mongodb://localhost:27017/proyecto-auth'
 };
