181e3d8bba053256b822094b37431a4d381b7652
mlarause <mlara681@misena.edu.co>
2025-05-19 01:49:56 -0500

    Update supplierRoutes.js

--- routes/supplierRoutes.js
+++ routes/supplierRoutes.js
@@ -3,15 +3,14 @@ const router = express.Router();
 const supplierController = require('../controllers/supplierController');
 const { authenticate, authorize } = require('../middlewares/auth');
 
-// Aplicar autenticación a todas las rutas
+// Todas las rutas requieren autenticación
 router.use(authenticate);
 
-// Endpoints
+// Endpoints específicos (SOLO MODIFICADO PARA PROVEEDORES)
 router.post('/', authorize(['admin', 'coordinador']), supplierController.createSupplier);
 router.get('/', authorize(['admin', 'coordinador', 'auxiliar']), supplierController.getSuppliers);
 router.get('/:id', authorize(['admin', 'coordinador', 'auxiliar']), supplierController.getSupplierById);
 router.put('/:id', authorize(['admin', 'coordinador']), supplierController.updateSupplier);
 router.delete('/:id', authorize(['admin']), supplierController.deleteSupplier);
-router.get('/product/:productId', authorize(['admin', 'coordinador', 'auxiliar']), supplierController.getSuppliersByProduct);
 
 module.exports = router;
