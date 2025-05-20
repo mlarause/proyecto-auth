f4beb4f11329017ae0d2c1e68843a35b81e8d7f7
mlarause <mlara681@misena.edu.co>
2025-05-19 23:55:57 -0500

    proveedor

--- middlewares/authJwt.js
+++ middlewares/authJwt.js
@@ -1,35 +1,53 @@
 const jwt = require('jsonwebtoken');
-const config = require('../config');
+const config = require('../config/auth.config');
+const db = require('../models');
+const User = db.user;
 
-exports.verifyToken = (req, res, next) => {
-    const token = req.headers['authorization'];
-
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; // Extraer token de Bearer
+    
     if (!token) {
-        return res.status(403).json({
+        return res.status(403).json({ 
             success: false,
-            message: 'Token no proporcionado'
+            message: "Token no proporcionado" 
         });
     }
 
-    jwt.verify(token.replace('Bearer ', ''), config.SECRET, (err, decoded) => {
+    jwt.verify(token, config.secret, (err, user) => {
         if (err) {
-            return res.status(401).json({
+            return res.status(401).json({ 
                 success: false,
-                message: 'Token inválido'
+                message: "Token inválido o expirado" 
             });
         }
-        req.userId = decoded.id;
-        req.userRol = decoded.rol; // IMPORTANTE: "rol" no "role"
+        req.user = user;
         next();
     });
 };
 
-exports.isAdmin = (req, res, next) => {
-    if (req.userRol !== 'admin') {
-        return res.status(403).json({
+const isAdmin = async (req, res, next) => {
+    try {
+        const user = await User.findByPk(req.user.id);
+        const roles = await user.getRoles();
+        
+        if (roles.some(role => role.name === 'admin')) {
+            return next();
+        }
+        
+        res.status(403).json({
             success: false,
-            message: 'Se requieren privilegios de administrador'
+            message: "Se requiere rol de administrador"
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error al verificar roles"
         });
     }
-    next();
+};
+
+module.exports = {
+    verifyToken,
+    isAdmin
 };
