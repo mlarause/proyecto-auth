61abdee9e86e329d19a5e3a7ec9b5b4bb0c56222
mlarause <mlara681@misena.edu.co>
2025-05-20 01:00:25 -0500

    proveedor

--- routes/supplierRoutes.js
+++ routes/supplierRoutes.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const supplierController = require("../controllers/supplierController");
 const authJwt = require("../middlewares/authJwt");
 
+// Admin: CRUD completo
 router.post("/", [authJwt.verifyToken, authJwt.isAdmin], supplierController.create);
 router.get("/", authJwt.verifyToken, supplierController.findAll);
 router.get("/:id", authJwt.verifyToken, supplierController.findOne);
 router.put("/:id", [authJwt.verifyToken, authJwt.isAdmin], supplierController.update);
 router.delete("/:id", [authJwt.verifyToken, authJwt.isAdmin], supplierController.delete);
 
-// Ruta adicional para coordinadores
+// Coordinador: Actualización parcial
 router.patch("/:id", [authJwt.verifyToken, authJwt.isCoordinator], supplierController.partialUpdate);
 
 module.exports = router;
