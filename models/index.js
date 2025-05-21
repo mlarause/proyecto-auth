686111e654cc180219ab0d115b0111af2e451e5c
mlarause <mlara681@misena.edu.co>
2025-05-20 21:01:22 -0500

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const User = require("./User");
 const Supplier = require("./Supplier");
 const Product = require("./Product");
-
+//db.supplier = require("./Supplier.js")(mongoose);
 module.exports = {
   User,
   Supplier,
